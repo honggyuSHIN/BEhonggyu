a248c079bcf5256ccfa182aace6401620772fb75
parkeunjin020404 <eunjin020404@naver.com>
2024-07-19 14:08:27 +0900

    installed apps에 corsheaders 추가

--- BE/settings.py
+++ BE/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
   
-
+    'corsheaders',
   
 
     'board',
@@ -66,7 +66,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
     'allauth.account.middleware.AccountMiddleware',
 ]
 
